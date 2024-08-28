3c2f3a040cf3de7d72e571dff579cad0c4da1978
BautistaBoselli <bboselli@fi.uba.ar>
2024-08-28 15:40:51 -0300

    fuerza por bash a ejecutar el script con 2 args

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Modo de uso: $0 <nombre_archivo_salida> <cantidad_clientes>"
+    exit 1
+fi
+
 echo "Nombre del archivo de salida: $1"
 echo "Cantidad de clientes: $2"
 python3 mi-generador.py $1 $2
